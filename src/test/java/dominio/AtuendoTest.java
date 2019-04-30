db63fdbfe95514089e8d434c8be94ab82c7141be
mlanneponders <mlyonadi@gmail.com>
2019-04-30 18:33:33 -0300

    Modificacion dependencia JUnit

---
+++ src/test/java/dominio/AtuendoTest.java
@@ -0,0 +1,20 @@
+package dominio;
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.BeforeEach;
+
+public class Test {
+
+    @BeforeEach
+    void init() {
+
+    }
+
+    @Test
+
+
+
+
+
+
+}
