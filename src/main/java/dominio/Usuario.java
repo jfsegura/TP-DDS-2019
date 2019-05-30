0599d6973d3b05e5219af1984828237b4c0948e6
gnfigueroa <gafigueroa@est.frba.utn.edu.ar>
2019-05-29 22:37:56 -0300

    Se modifica Usuario.

--- src/main/java/dominio/Usuario.java
+++ src/main/java/dominio/Usuario.java
@@ -4,8 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Usuario {
+    private String nombre;
+    private String apellido;
 
     private List<Guardarropa> guardarropas = new ArrayList<Guardarropa>();
+
+
+    public Usuario(String nombre,String apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+      }
+
+    public String getNombre(){
+        return nombre;
+    }
+    public String getApellido(){
+        return apellido;
+    }
+
+    private void agregarGuardarropa(Guardarropa guardarropa){
+        this.guardarropas.add(guardarropa);
+
+    }
+
+
 
 
 
